804969e4fd9c56316fbb525660077f3a688f9176
Merna <merna.wahdan@gmail.com>
2023-08-23 14:11:07 +0200

    implement product pages (hbs, layout, views)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
 // Make everything inside of public/ available
 app.use(express.static('public'));
 
@@ -12,15 +15,67 @@ app.listen(3000, () => {
 
 //Contact
 app.get("/contact", (request, response, next) => {
-    // console.log(request.protocol)
-    // console.log(request.query)
-    // console.log("we recieved a request page 5");
-    response.sendFile(__dirname + '/views/contact.html');
+    response.render("contact")
 })
 
 //Home page
 app.get('/', (request, response, next) => {
-        // console.log(request.protocol)
+    response.render("homepage")
+})
 
-    response.sendFile(__dirname + '/views/homepage.html');
+//Creating new Routes for types of pizzas
+app.get("/pizzas/margarita", (request, response, next) => {
+    const dataMargarita = {
+        title: "Pizza Margarita",
+        amount: 10,
+        imageFile: "/images/margarita.jpg",
+        ingredients: [
+            {
+                ingredientName: "mozzarella",
+                calories: 400
+            },
+            {
+                ingredientName: "tomato sauce",
+                calories: 200
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 30
+            },
+          ],
+    }
+    response.render("product", dataMargarita)
+})
+
+app.get("/pizzas/veggie", (request, response, next) => {
+    const dataVeggie = {
+        title: "Pizza Veggie",
+        amount: 12,
+        imageFile: "/images/veggie.jpg",
+        ingredients: [
+            {
+                ingredientName: "cherry tomatoes",
+                calories: 80
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 30
+            },
+            {
+                ingredientName: "olives",
+                calories: 300
+            },
+          ],
+    }
+    response.render("product", dataVeggie)
+})
+
+app.get("/pizzas/seafood", (request, response, next) => {
+    const dataSea = {
+        title: "Pizza Seafood",
+        // amount: 15,
+        imageFile: "/images/seafood.jpg",
+        // ingredients: ['tomato sauce', 'garlic', 'prawn'],
+    }
+    response.render("product", dataSea)
 })
